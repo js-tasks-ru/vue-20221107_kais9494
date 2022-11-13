440669a034ff34f56bcd1b4e51d3e3a65a3fda98
dima_vch <109754944+DimaVoiceh@users.noreply.github.com>
2022-11-14 01:19:16 +0300

    01-basics
    Решения задач

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,30 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+
+const app = createApp({
+    data() {
+        return {
+            a: 0,
+            b: 0,
+            action: '',
+        }
+    },
+    computed: {
+        result() {
+            switch (this.action) {
+                case 'sum':
+                    return this.a + this.b
+                    break;
+                case 'subtract':
+                    return this.a - this.b
+                    break;
+                case 'multiply':
+                    return this.a * this.b
+                    break;
+                case 'divide':
+                    return this.a / this.b
+                    break;
+            }
+        }
+    }
+}).mount('#app');
